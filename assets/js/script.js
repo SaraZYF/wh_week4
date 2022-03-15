f35e91ad1c2bd570ad253bdf3d47b5251e245b6d
SaraZYF <60347364+SaraZYF@users.noreply.github.com>
2022-03-15 18:34:40 +1100

    draft 2.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,119 @@
-//select option and return//
-const ul_1=document.querySelector(".option1");
-const ul_2=document.querySelector(".option2");
+//list questions below
 
-//hide the first quiz and show next one//
+const Questions = [{
+    id: 0,
+    q: "What is 10+5?",
+    a: [{text: "5", isCorrect: false},
+        {text: "15", isCorrect: true},
+        {text: "3", isCorrect: false},
+        {text: "25", isCorrect: false}
+]},
 
-const q1=document.querySelector(".q1");
-const q2=document.querySelector(".q2");
+    {id: 1,
+    q: "What is 10*5?",
+    a: [{text: "5", isCorrect: false},
+        {text: "15", isCorrect: false},
+        {text: "3", isCorrect: false},
+        {text: "50", isCorrect: true}
+        ]
+    }
+]
+
+console.log(Questions[0]);
 
 //complete quiz, hide and show end//
 
-const quiz = document.querySelector(".quiz");
-const end = document.querySelector(".end");
-
-//select result for each quiz//
-const results=document.querySelector('.results');
-//const choice=document.querySelector('li');
-const select =document.querySelector('#a1');
+// const quiz = document.querySelector(".quiz");
+// const end = document.querySelector(".end");
 
  
-// Quiz click change//
-//click correct result, shows result, click the wrong result shows "wrong"//
-//when result=correct, click again, jump to next question//
-//count, right result, when right result >1, move to next quiz//
-ul_1.addEventListener ("click", function () {
+//Set Start
+var start =true;
 
- 
-select.onclick = function updateresult (){
-    results.textContent='Correct!';
+//Interate
+function iterate(id) {
+
+    //show result
+    var result =document.getElementsByClassName ("result");
+    result[0].innerText="";
+
+    //define question
+    const question = document.getElementById("quiz");
+
+    //show question text
+    question.innerText=Questions[id].q;
+
+    //define options
+    const op1 = document.getElementById ('op1');
+    const op2 = document.getElementById ('op2');
+    const op3 = document.getElementById ('op3');
+    const op4 = document.getElementById ('op4');
+
+    //show options text
+     op1.innerText = Questions[id].a[0].text;
+     op2.innerText = Questions[id].a[1].text;
+     op3.innerText = Questions[id].a[2].text;
+     op4.innerText = Questions[id].a[3].text;
+
+
+    //show true or false value for options
+    op1.value = Questions[id].a[0].isCorrect;
+    op2.value = Questions[id].a[1].isCorrect;
+    op3.value = Questions[id].a[2].isCorrect;
+    op4.value = Questions[id].a[3].isCorrect;
+
+var seleted="";
+
+   //add correct value for options
+   op1.addEventListener ("click", () => {
+    seleted = op1.value;
+    if (seleted =="true"){
+    result[0].innerHTML ="Correct";}
+    else {
+        result[0].innerHTML ="Wrong";}
+})
+
+    //add correct value for options
+    op2.addEventListener ("click", () => {
+        seleted = op2.value;
+        if (seleted =="true"){
+        result[0].innerHTML ="Correct";}
+        else {
+            result[0].innerHTML ="Wrong";}
+    })
+
+    //add correct value for options
+   op3.addEventListener ("click", () => {
+    seleted = op3.value;
+    if (seleted =="true"){
+    result[0].innerHTML ="Correct";}
+    else {
+        result[0].innerHTML ="Wrong";}
+})
+
+   //add correct value for options
+   op4.addEventListener ("click", () => {
+    seleted = op1.value;
+    if (seleted =="true"){
+    result[0].innerHTML ="Correct";}
+    else {
+        result[0].innerHTML ="Wrong";}
+})
+
 }
 
-// if (choice === '3. quotes'){
-// //     //right answer select
-//     results.textContent='Correct!';
-// //    //count the time of right selected time >0
-         //need to consider event bubble here
-// //    //display message
-q1.style.display ="none";
-q2.style.display="block";
+    if (start) {
+        iterate("0");
+    }
 
-// else {
-//     results.textContent='Wrong!';
-//     //wrong answer select
-//     //display message, loop 
-// }
 
-});
 
-ul_2.addEventListener ("click", function () {
-    q2.style.display ="none";
-    end.style.display="block";
-});
+
+// this still neeed, as it is complete quiz and show last page
+// question.id:2.addEventListener ("click", function () {
+//     quiz.style.display ="none";
+//     end.style.display="block";
+// });
 
 //Quiz Result//
 
